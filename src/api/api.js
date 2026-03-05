922fc9c9a4acb2910ff76363333ce202c3db0f0c
alameen-47 <130351217+alameen-47@users.noreply.github.com>
2026-03-05 03:23:40 +0300

    fixed issue with the react-native-bluetooth-classic on ios build

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 // Use 10.0.2.2 for Android emulator, localhost or your IP for iOS simulator or physical device
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.116:8000',
+  baseURL: 'http://192.168.1.109:8000',
 });
 
 export default api;
